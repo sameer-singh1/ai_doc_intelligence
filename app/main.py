3c8d2bdfafb6d017bfa616b420766a06b7fad54d
Sameer <sameer.singh@kaaratech.com>
2026-03-02 15:07:01 +0530

    feat: add protected route using jwt dependency

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from app.database.connection import engine
 from app.database.models import Base
 from app.api import auth
+from app.core.auth_dependency import get_current_user
 
 app = FastAPI()
 
@@ -12,3 +13,7 @@ app.include_router(auth.router)
 @app.get("/")
 def home():
     return {"message": "Backend + Auth Running"}
+
+@app.get("/protected")
+def protected_route(current_user = Depends(get_current_user)):
+    return {"message": f"Hello {current_user.email}"}
